39677ae4081b8d66d26ca109b887e11926134106
Alexander Prikhodko <alaront.work@gmail.com>
2024-07-27 17:09:09 +0300

    Base style and font

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
+  <h1>Test</h1>
 </template>
 
 <style scoped lang="scss">
+  @import "./assets/style/variables";
+
+  h1 {
+    text-align: center;
+    color: $main-red;
+    font-family: $font-inter;
+  }
 </style>
